197e306ea51fb0a98d4fd61677b8dfad130de0d9
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-10-18 18:31:08 +0200

    Maek compil

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,26 @@
 #![no_main]
 #![feature(type_alias_impl_trait)]
 
+use core::sync::atomic::{AtomicU32, Ordering};
+
 use defmt::info;
 use embassy_executor::Spawner;
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Input, Pull};
 use {defmt_rtt as _, panic_probe as _};
 
+static COUNTER: AtomicU32 = AtomicU32::new(0);
+
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello World!");
 
-    let button = Input::new(p.PC13, Pull::Down);
-    let mut button = ExtiInput::new(button, p.EXTI13);
-
-    info!("Press the USER button...");
+    let pa0 = Input::new(p.PA0, Pull::Down);
+    let mut pa0 = ExtiInput::new(pa0, p.EXTI0);
 
     loop {
-        button.wait_for_rising_edge().await;
-        info!("Pressed!");
-        button.wait_for_falling_edge().await;
-        info!("Released!");
+        pa0.wait_for_rising_edge().await;
+        COUNTER.fetch_add(1, Ordering::Relaxed);
     }
 }
