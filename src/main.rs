d2eff66da5f9dca45ca84a9f0ac3debd79560fa3
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-10-20 14:14:38 +0200

    Add uart

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@
 use core::sync::atomic::{AtomicU32, Ordering};
 
 use embassy_executor::Spawner;
+use embassy_stm32::dma::NoDma;
 use embassy_stm32::exti::ExtiInput;
 use embassy_stm32::gpio::{Input, Pull};
+use embassy_stm32::interrupt::{self};
 use embassy_stm32::peripherals::{PA0, PA1, PA2};
+use embassy_stm32::usart::{BufferedUart, Config, State, Uart};
+use embedded_io::asynch::{BufRead, Write};
+use rfb_proto::Message;
 use {defmt_rtt as _, panic_probe as _};
 
 static COUNTER: AtomicU32 = AtomicU32::new(0);
@@ -15,6 +20,16 @@ static COUNTER: AtomicU32 = AtomicU32::new(0);
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
+
+    let mut config = Config::default();
+    config.baudrate = 9600;
+    let usart = Uart::new(p.USART1, p.PA10, p.PA9, NoDma, NoDma, config);
+
+    let mut state = State::new();
+    let irq = interrupt::take!(USART1);
+    let mut tx_buf = [0u8; 32];
+    let mut rx_buf = [0u8; 1];
+    let mut buf_usart = BufferedUart::new(&mut state, usart, irq, &mut tx_buf, &mut rx_buf);
 
     let pa0 = Input::new(p.PA0, Pull::Up);
     let pa1 = Input::new(p.PA1, Pull::Up);
@@ -31,6 +46,21 @@ async fn main(spawner: Spawner) {
     spawner.spawn(pa0_hdl).unwrap();
     spawner.spawn(pa1_hdl).unwrap();
     spawner.spawn(pa2_hdl).unwrap();
+
+    let (mut rx, mut tx) = buf_usart.split();
+    loop {
+        let buf = rx.fill_buf().await.unwrap();
+        let n = buf.len();
+        let byte = rfb_proto::from_bytes(buf);
+        rx.consume(n);
+        if let Ok(Message::Request) = byte {
+            let count = COUNTER.swap(0, Ordering::Acquire);
+            let response = Message::Response(count as u64);
+            let bytes: rfb_proto::Vec<u8, 9> = rfb_proto::to_vec(&response).unwrap();
+            tx.write_all(&bytes).await.unwrap();
+            tx.flush().await.unwrap();
+        }
+    }
 }
 
 #[embassy_executor::task]
